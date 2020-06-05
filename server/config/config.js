30a1832aa7e32c96d00262ab0063361996f30b08
Jorge <jbaigorria0@gmail.com>
2020-06-05 09:27:47 -0300

    RestServer - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -2,3 +2,24 @@
 // Puerto
 //================================================
 process.env.PORT = process.env.PORT || 3000;
+
+//================================================
+// Entorno
+//================================================
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//================================================
+// Base de datos
+//================================================
+let urlBD;
+
+if (process.env.NODE_ENV === 'dev') {
+
+    urlBD = 'mongodb://localhost:27017/cafe';
+
+} else {
+
+    urlBD = 'mongodb+srv://JDBaigorria:1oYTkYZTcQ9HqkNm@cluster0-emcf1.mongodb.net/cafe'
+}
+
+process.env.URLDB = urlBD;
